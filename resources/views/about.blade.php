efb161b9a519ab3f91512c5e9b489ffb7d62aec8
fturmhrm <fathurmilqimuharom@smkwikrama.sch.id>
2024-06-13 16:46:21 +0700

    laravel 1

---
+++ resources/views/about.blade.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <header class="header">
+        <a href="#" class="logo">FMM.</a>
+        <nav class="navbar">
+            <a href="http://127.0.0.1:8000/home" class="active">Home</a>
+            <a href="">About</a>
+            <a href="#education ">Education</a>
+            <a href="#skills">Skilss</a>
+            <a href="#contact">Contact</a>
+        </nav>
+    </header>
+
+    <section class="about" id="about">
+        <h2 class="heading">About <span>Me</span></h2>
+
+        <div class="about-img">
+            <img src="img/poto.jpeg">
+            <span class="circle-spin"></span>
+        </div>
+
+        <div class="about-content">
+            <h3>Frontend Developer!</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates deserunt doloremque debitis quidem
+                vel, tenetur tempora necessitatibus praesentium dignissimos delectus. Repudiandae cupiditate ipsum
+                tempora eius mollitia sed maiores nisi expedita, enim, sunt magni neque, magnam pariatur qui accusantium
+                nihil. Est et, tenetur dolor fuga blanditiis magnam optio facere possimus cum.</p>
+
+            <div class="btn-box btns">
+                <a href="#" class="btn">Read More</a>
+            </div>
+        </div>
+    </section>
+
+    <section class="education" id="education">
+        <h2 class="heading">My <span>Journey</span></h2>
+
+        <div class="education-row">
+            <div class="education-column">
+                <h3 class="title">Education</h3>
+
+                <div class="education-box">
+                    <div class="education-content">
+                        <div class="content">
+                            <div class="year"><i class='bx bxs-calendar'></i>2023-NOW</div>
+                            <h3>SMK WIKRAMA BOGOR</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem error aliquam sint quaerat
+                                doloremque, a aperiam omnis voluptatibus delectus dolorum.</p>
+                        </div>
+                    </div>
+
+                    <div class="education-content">
+                        <div class="content">
+                            <div class="year"><i class='bx bxs-calendar'></i>2020-2023</div>
+                            <h3>SMP IT-ARRAHMAH</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem error aliquam sint quaerat
+                                doloremque, a aperiam omnis voluptatibus delectus dolorum.</p>
+                        </div>
+                    </div>
+
+                    <div class="education-content">
+                        <div class="content">
+                            <div class="year"><i class='bx bxs-calendar'></i>2014-2020</div>
+                            <h3>SDN CIPAKU PERUMDA</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem error aliquam sint quaerat
+                                doloremque, a aperiam omnis voluptatibus delectus dolorum.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="education-column">
+                <h3 class="title">Experience</h3>
+
+                <div class="education-box">
+                    <div class="education-content">
+                        <div class="content">
+                            <div class="year"><i class='bx bxs-calendar'></i>2023-NOW</div>
+                            <h3>Web Developer</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem error aliquam sint quaerat
+                                doloremque, a aperiam omnis voluptatibus delectus dolorum.</p>
+                        </div>
+                    </div>
+
+                    <div class="education-content">
+                        <div class="content">
+                            <div class="year"><i class='bx bxs-calendar'></i>2023-NOW</div>
+                            <h3>Web Developer</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem error aliquam sint quaerat
+                                doloremque, a aperiam omnis voluptatibus delectus dolorum.</p>
+                        </div>
+                    </div>
+
+                    <div class="education-content">
+                        <div class="content">
+                            <div class="year"><i class='bx bxs-calendar'></i>2023-NOW</div>
+                            <h3>Web Developer</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem error aliquam sint quaerat
+                                doloremque, a aperiam omnis voluptatibus delectus dolorum.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- skill section design -->
+    <section class="skills" id="skills">
+        <h2 class="heading">My <span>Skills</span></h2>
+
+        <div class="skills-row">
+            <div class="skills-column">
+                <h3 class="title">Coding Skills</h3>
+
+                <div class="skills-box">
+                    <div class="skills-content">
+                        <div class="progress">
+                            <h3>HTML <span>90%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+
+                        <div class="progress">
+                            <h3>CSS <span>80%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+
+                        <div class="progress">
+                            <h3>JavaScript <span>0%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+
+                        <div class="progress">
+                            <h3>Python <span>0%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="skills-column">
+                <h3 class="title">Profesional Skills</h3>
+
+                <div class="skills-box">
+                    <div class="skills-content">
+                        <div class="progress">
+                            <h3>Web design <span>45%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+
+                        <div class="progress">
+                            <h3>Web development <span>40%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+
+                        <div class="progress">
+                            <h3> Cooking<span>0%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+
+                        <div class="progress">
+                            <h3>Marketing <span>80%</span></h3>
+                            <div class="bar"><span></span></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- contact section design -->
+    <section class="contact" id="contact">
+        <h2 class="heading">Contact <span>Me!</span></h2>
+
+        <form action="#">
+            <div class="input-box">
+                <div class="input-field">
+                    <input type="text" placeholder="Full Name" required>
+                    <span class="focus"></span>
+                </div>
+                <div class="input-field">
+                    <input type="text" placeholder="Email Address" required>
+                    <span class="focus"></span>
+                </div>
+            </div>
+
+            <div class="input-box">
+                <div class="input-field">
+                    <input type="number" placeholder="Mobile Number" required>
+                    <span class="focus"></span>
+                </div>
+                <div class="input-field">
+                    <input type="text" placeholder="Email Subject" required>
+                    <span class="focus"></span>
+                </div>
+            </div>
+
+            <div class="textarea-field">
+                <textarea name="" id="" cols="30" rows="10" placeholder="Your Message" required></textarea>
+                <span class="focus"></span>
+            </div>
+
+            <div class="btn-box btns">
+                <button type="submit" class="btn">Submit</button>
+            </div>
+        </form>
+    </section>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+
+
+    <section class="home">
+        <div class="home-content">
+            <h1>Hi I'm <span>Fathur Milqi Muharom</span></h1>
+            <div class="text-animate">
+                <h3>Frontend Developer</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias quos eligendi aspernatur repellat
+                quisquam sapiente, impedit a veniam doloribus aliquam excepturi odio accusamus, aperiam pariatur!</p>
+
+            <div class="btn-box">
+                <a href="http://127.0.0.1:8000/about" class="btn">About Me</a>
+            </div>
+
+        </div>
+
+        <div class="home-sci">
+            
+                <a href="#"><i class='bx bxl-tiktok'></i></a></li>
+                <a href="https://www.instagram.com/ftur_mhrm/"><i class='bx bxl-instagram'></i></a></li>
+                <a href="https://wa.me/+6287860165577"><i class='bx bxl-whatsapp'></i></a></li>
+            
+        </div>
+
+        <div class="home-imgHover"></div>
+    </section>
+</body>
+</html>
